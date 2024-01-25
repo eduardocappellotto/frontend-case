3c9e72ad052c0746457f96601f072f4e37598fc5
Eduardo <eduardocappellotto@gmail.com>
2024-01-25 14:26:16 -0300

    feat: :art: inserts scss/sass on the project

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <NuxtLayout />
+    <h1>Teste</h1>
 </template>
+
+<style lang="scss" scoped>
+h1 {
+    color: $primary-base;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  css: ["@/assets/scss/global.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/_colors.scss" as *;'
+        }
+      }
+    }
+  }
 })
