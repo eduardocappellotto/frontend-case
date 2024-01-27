51e61bfd3843cb6cbd769a4a5433a8753132e451
Eduardo <eduardocappellotto@gmail.com>
2024-01-27 20:18:49 -0300

    feat: :sparkles: Created a Header and a Button component
    Created a header that also uses the universal JButton component

---
+++ components/JButton.vue
@@ -0,0 +1,81 @@
+<template>
+    <button @click="emit('click')" class="j-button" :class="{ 'col-reverse': revert, [`j-button--${type}`]: type }">
+        <div class="j-button__left">
+            <Icon v-if="props.icon" :name="props.icon" size="1.5rem" />
+            <div class="mx-1"></div>
+            <span class="j-button__text">{{ text }}</span>
+        </div>
+
+    </button>
+</template>
+  
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    icon: String,
+    revert: Boolean,
+    text: String,
+    type: String,
+});
+
+const emit = defineEmits(['click']);
+
+</script>
+  
+<style scoped lang="scss">
+.j-button {
+    display: flex;
+    justify-content: space-between;
+
+    font-family: "Inter";
+    border-radius: 0.5rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-weight: 500;
+    font-size: 14px;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    transition: background-color 0.2s;
+
+    &__tip {
+        background-color: $primary-base;
+        position: absolute;
+        left: -10px;
+        border-radius: 25%;
+    }
+
+    &.col-reverse {
+        flex-direction: row-reverse;
+
+        .j-button__left {
+            flex-direction: row-reverse;
+        }
+    }
+
+    .j-button__left {
+        display: flex;
+        align-items: center;
+    }
+
+    &--secondary {
+        background-color: $primary-base;
+        border: none;
+        color: $bg-weak-100;
+    }
+
+    &--primary {
+        background-color: transparent;
+        box-sizing: border-box;
+        border: 1px solid $stroke-soft-200;
+        color: $text-sub-500;
+    }
+
+    &:hover {
+        opacity: 0.8;
+    }
+}
+</style>
+
---
+++ components/JHeader.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="j-header mt-2 mb-10 mx-10">
+        <div class="j-header__usercontent">
+            <img class="j-header__usercontent__avatar" src="/img/avatar.png" alt="User Photo" />
+            <div class="j-header__usercontenttext ml-4">
+                <span class="j-header__usercontenttext__username mb-1">Sophia Williams</span>
+                <span class="j-header__usercontenttext__welcomeback"> Bem-vinda de volta 👋</span>
+            </div>
+        </div>
+
+        <div class="j-header__buttons">
+            <Icon class="mx-6" name="ri:search-2-line" size="1.5rem" color="#525866" />
+            <Icon class="mx-6" name="streamline:notification-alarm-2" size="1.5rem" color="#525866" />
+            <JButton class="pa-2 mx-3" icon="ri:calendar-check-line" :revert="false" text="Calendário" type="primary"
+                font-size="14px" />
+            <JButton class="pa-2" icon="ri:add-line" text="Nova Solicitação" type="secondary" font-size="14px" />
+        </div>
+    </div>
+</template>
+<style scoped lang="scss">
+.j-header {
+
+    width: 100%;
+    height: 88px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    & .j-header__usercontent {
+        display: flex;
+
+        & .j-header__usercontenttext {
+            display: flex;
+            flex-direction: column;
+
+            & .j-header__usercontenttext__username {
+                font-family: Inter;
+                font-size: 18px;
+                font-weight: 500;
+                line-height: 24px;
+                letter-spacing: -0.015em;
+                text-align: left;
+            }
+
+            & .j-header__usercontexttext__welcomeback {
+                font-family: Inter;
+                font-size: 14px;
+                font-weight: 400;
+                line-height: 20px;
+                letter-spacing: -0.006em;
+                text-align: left;
+
+            }
+        }
+    }
+
+    &__buttons {
+        display: flex;
+        align-items: center;
+    }
+
+}
+</style>
